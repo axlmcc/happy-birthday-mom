b43b6c367f4aeea3cbb683190a6b58c04146b1ac
axlc <axl.mccracken@gmail.com>
2020-06-16 00:32:11 +0000

    Update styles and wait before animating giraffe

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,34 @@
 import '../styles/main.scss';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-var camera, scene, renderer, loader, controls;
+var camera, scene, renderer, loader, material;
 
 init();
-animate();
+setTimeout( () => {
+    // Wait 3 seconds, then animate
+    animate();
+}, 3 * 1000);
 
 function init() {
     var aspectRatio = window.innerWidth / window.innerHeight;
+    window.addEventListener( 'resize', setAspectRatio, false);
 
-    camera = new THREE.PerspectiveCamera( 10, aspectRatio, 0.1, 2000 );
+    camera   = new THREE.PerspectiveCamera( 10, aspectRatio, 0.1, 2000 );
     scene    = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer( { alpha: true } );
     loader   = new GLTFLoader();
-    controls = new OrbitControls( camera, renderer.domElement );
+    material = new THREE.MeshNormalMaterial({});
 
-    scene.background = new THREE.Color(0xdddddd);
+    // scene.background = new THREE.Color(0xdddddd);
 
     var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
     var pointLight   = new THREE.PointLight(0xffffff, 0.8);
     scene.add(ambientLight);
     camera.add(pointLight);
 
-    camera.position.z = 1;
-    controls.update();
+    camera.position.z = 2;
+    camera.position.y = 0.25;
 
     renderer.outputEncoding = THREE.sRGBEncoding;
 
@@ -68,10 +71,9 @@ function init() {
 function animate() {
     requestAnimationFrame( animate );
 
-    if (camera.position.y >= -0.4) {
-        camera.position.y -= 0.005;
+    if (camera.position.y >= -0.1) {
+        camera.position.y -= 0.0005;
     }
-    controls.update();
 
     render();
 }
@@ -79,8 +81,18 @@ function animate() {
 function render() {
     scene.traverse( function(o) {
         if (!o.isMesh) return;
-        o.material = new THREE.MeshNormalMaterial({});
+        o.material = material;
     });
 
     renderer.render( scene, camera );
+}
+
+function setAspectRatio() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var aspectRatio = width / height;
+
+    camera.aspect = aspectRatio;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight);
 }
